59ed5405965e7216488767da5c00594899eaf299
greff <vitorgreffdias@gmail.com>
2026-02-13 19:19:52 -0300

    move/wrap directions on neovim buffers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,6 +9,30 @@ vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("n", "<leader>w", ":w!<CR>", { desc = "Force write" })
 vim.keymap.set("n", "<leader>q", ":q!<CR>", { desc = "Force quit" })
 
+-- Window navigation with wrapping in all directions
+local function wrap_window(direction, opposite)
+	local current_win = vim.api.nvim_get_current_win()
+	vim.cmd("wincmd " .. direction)
+	local new_win = vim.api.nvim_get_current_win()
+	if current_win == new_win then
+		-- Hit edge, wrap to opposite side
+		vim.cmd("wincmd " .. opposite)
+	end
+end
+
+vim.keymap.set("n", "<C-w>l", function()
+	wrap_window("l", "h")
+end, { noremap = true, desc = "Move right or wrap to left" })
+vim.keymap.set("n", "<C-w>h", function()
+	wrap_window("h", "l")
+end, { noremap = true, desc = "Move left or wrap to right" })
+vim.keymap.set("n", "<C-w>j", function()
+	wrap_window("j", "k")
+end, { noremap = true, desc = "Move down or wrap to top" })
+vim.keymap.set("n", "<C-w>k", function()
+	wrap_window("k", "j")
+end, { noremap = true, desc = "Move up or wrap to bottom" })
+
 vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = "YankHighlight",
